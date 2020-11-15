1c33a865b249713c1fd14bf45c89fa5490979cd2
drgt344 <drgt344@hotmail.com>
2020-11-15 19:54:26 +0100

    Podstawy quizzu

---
+++ Quizz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rewards = new int[] { 500, 1000, 2000, 5000, 10000, 20000, 40000, 75000, 125000, 250000, 500000, 1000000 };
+            bool[] helpUsed = new bool[] { false, false, false };
+            Question[] questions;
+
+            Console.WriteLine("************************************");
+            Console.WriteLine("****************Quizz***************");
+            Console.WriteLine("************************************");
+
+            Console.ReadLine();
+        }
+
+        static int LoadQuestions()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Quizz/Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizz
+{
+    class Question
+    {
+        public string qtext;
+        public string[] answers = new string[4];    //opcja 0 jest prawidłowa
+    }
+}
